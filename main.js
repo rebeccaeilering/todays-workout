30fa41033297d9810bb161b49d26b8f7463203e6
Rebecca Eilering <rebeccaeilering@gmail.com>
2019-02-21 22:46:24 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,94 @@
+const workouts = [
+  "walking",
+  "jogging",
+  "squats",
+  "walking lunges",
+  "squat jumps",
+  "high knees",
+  "bicycle",
+  "eliptical",
+  "burpees",
+  "pushups",
+  "skaters",
+  "basketball",
+  "jumping jacks",
+  "abdominal",
+  "weights",
+  "sprints",
+  "kettle bell swings",
+  "bent over rows",
+  "shadow boxing"
+];
+
+const abdominal = [
+  "knee to elbow crunches",
+  "flutter kicks",
+  "scissors",
+  "reverse crunches",
+  "sitting twists",
+  "mountain climbers",
+  "planks",
+  "plank jacks",
+  "situps",
+  "leg raises",
+  "high crunches",
+  "knee crunches",
+  "back extensions",
+  "v ups"
+];
+
+const weights = [
+  "rows",
+  "bicep curl",
+  "bench press",
+  "shoulder press",
+  "lat pulldowns",
+  "leg press",
+  "tricep pushdown",
+  "leg curls",
+  "knee extension",
+  "push up",
+  "squat",
+  "calf raise",
+  "dumbell row",
+  "kettle bell swings"
+];
+
+const list = document.getElementById('workouts');
+const abContain = document.getElementById('ab-container');
+const weightContain = document.getElementById('weight-container');
+
+for (let i = 0; i < workouts.length; i++) {
+  const workout = document.createElement("li");
+  workout.innerHTML = workouts[i];
+  workout.setAttribute('data-workouts', workouts[i]);
+  list.appendChild(workout);
+}
+
+for (let i = list.children.length; i >= 0; i--) {
+  list.appendChild(list.children[Math.random() * i | 0]);
+}
+
+const dataAbs = document.querySelector('li[data-workouts="abdominal"]');
+const dataWeights = document.querySelector('li[data-workouts="weights"]');
+
+if(dataAbs.offsetWidth !== 0 || dataAbs.offsetHeight !== 0) {
+  const abList = document.getElementById('ab-workouts');
+  for (let i = 0; i < abdominal.length; i++) {
+    const item = document.createElement("li");
+    item.innerHTML = abdominal[i];
+    abList.appendChild(item);
+  }
+  abContain.style.display = 'block';
+}
+
+if(dataWeights.offsetWidth !== 0 || dataWeights.offsetHeight !== 0) {
+  const weightList = document.getElementById('weight-workouts');
+  for (let i = 0; i < weights.length; i++) {
+    const item = document.createElement("li");
+    item.innerHTML = weights[i];
+    weightList.appendChild(item);
+  }
+  weightContain.style.display = 'block';
+
+}
